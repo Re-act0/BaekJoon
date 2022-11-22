9179f3e41590ad5958be127055fddf0928759557
re_act <re-act@naver.com>
2022-11-22 18:23:40 +0900

    2022-11-22

---
+++ src/BaekJoon_11653.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class BaekJoon_11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        int n = Integer.parseInt(br.readLine());
+        int i = 2;
+
+        while (n > 1) { // n이 1이 되기 전까지 반복
+            if (n % i == 0) { // 만약 2로 나누어 지면 출력 후 한번 더 반복
+                n = (n / i); // n을 나눈 몫을 다시 n에 저장
+                bw.write(i + "\n"); // 현재 나눈 i를 출력
+            } else if (n % i != 0) { // 만약 나누어 떨어지지 않으면 i를 증가시킴
+                i++;
+            }
+        }
+
+        bw.flush();
+    }
+}
+
