7b6b0f71d1b0333e8801810faebc704c6225066a
re_act <re-act@naver.com>
2022-11-27 16:36:36 +0900

    2022-11-27

---
+++ src/BaekJoon_1085.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BaekJoon_1085 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int min = x;
+
+        int arr[] = {x, y, w - x, h - y};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min >= arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        bw.write(min + "");
+
+        bw.flush();
+    }
+
+
+}
