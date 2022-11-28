c3dfc3f798f84403a2236e65dcd1c95b119a67ec
re_act <re-act@naver.com>
2022-11-28 20:17:19 +0900

    2022-11-28

---
+++ src/BaekJoon_4153.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BaekJoon_4153 { // 4153번
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            if (x == 0 && y == 0 && z == 0) {
+                break;
+            }
+            if ((x > y) && (x > z)) {
+                if ((x * x) == (y * y) + (z * z)) {
+                    bw.write("right\n");
+                } else {
+                    bw.write("wrong\n");
+                }
+            } else if ((y > x) && (y > z)) {
+                if ((y * y) == (x * x) + (z * z)) {
+                    bw.write("right\n");
+                } else {
+                    bw.write("wrong\n");
+                }
+            } else if ((z > x) && (z > y)) {
+                if ((z * z) == (x * x) + (y * y)) {
+                    bw.write("right\n");
+                } else {
+                    bw.write("wrong\n");
+                }
+            }
+        }
+        bw.flush();
+    }
+
+
+}
