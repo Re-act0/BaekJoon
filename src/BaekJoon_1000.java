433a007493f43f81ec2b0f0f712822a306fdd5f0
re_act <re-act@naver.com>
2022-11-29 17:30:10 +0900

    A + B / 2022-10-22

--- src/BaekJoon_1000.java
+++ src/BaekJoon_1000.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BaekJoon_1000 { // A+B
+public class BaekJoon_1000 {  // A+B
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
