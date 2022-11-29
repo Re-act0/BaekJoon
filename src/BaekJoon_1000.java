9e47ecfc53800d54ebfdff18c6158da09bab1a31
re_act <re-act@naver.com>
2022-11-29 17:31:10 +0900

    A + B / 2022-10-22

--- src/BaekJoon_1000.java
+++ src/BaekJoon_1000.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
-public class BaekJoon_1000 {  // A+B
-
+public class BaekJoon_1000 { // A+B
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum;
         int A = sc.nextInt();
         int B = sc.nextInt();
-        sum = A+B;
+        sum = A + B;
         System.out.println(sum);
 
     }
