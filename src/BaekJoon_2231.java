968cbe977a7c565dfde3b5a2ac467fa1917526b1
re_act <re-act@naver.com>
2022-11-29 16:13:05 +0900

    2022-11-29

---
+++ src/BaekJoon_2231.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class BaekJoon_2231 { // 2231번
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int res = 0;
+        int a = Integer.parseInt(br.readLine()); // a를 입력받는다
+
+        for (int i = 0; i < a; i++) {
+            String s = Integer.toString(i);
+            res = i;
+            for (int j = 0; j < s.length(); j++) {
+                res += Character.getNumericValue(s.charAt(j));
+            }
+            if (res == a){
+                bw.write(i + "");
+                break;
+            }
+            else if ((i == a-1) && (res!=a)){
+                bw.write("0");
+            }
+        }
+
+
+        bw.flush();
+    }
+
+}
