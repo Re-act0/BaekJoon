f5efb526582f2685aaa8140dd926615fac68b059
re_act <re-act@naver.com>
2022-11-30 19:33:54 +0900

    더하기 사이클 / 2022-10-29

--- src/BaekJoon_1110.java
+++ src/BaekJoon_1110.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class BaekJoon_1110 { // 더하기 사이클
+public class BaekJoon_1110 {  // 더하기 사이클
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
