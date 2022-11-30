7c50d2035f63ab939be6eb06be20124198ace342
re_act <re-act@naver.com>
2022-12-01 00:25:46 +0900

    사파리월드 / 2022-10-26

--- src/BaekJoon_2420.java
+++ src/BaekJoon_2420.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BaekJoon_2420 { // 사파리월드
+public class BaekJoon_2420 {  // 사파리월드
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
