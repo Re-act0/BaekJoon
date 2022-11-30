c5b78082c7b0f40a4908664544f31899fc69dfe9
re_act <re-act@naver.com>
2022-12-01 00:18:24 +0900

    평균 / 2022-11-02

--- src/BaekJoon_1546.java
+++ src/BaekJoon_1546.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class BaekJoon_1546 { // 평균
+public class BaekJoon_1546 {  // 평균
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
