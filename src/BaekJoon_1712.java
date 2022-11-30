f0e878233a3def7e57bee11cd48225c95da79d85
re_act <re-act@naver.com>
2022-12-01 00:19:08 +0900

    손익분기점 / 2022-11-13

--- src/BaekJoon_1712.java
+++ src/BaekJoon_1712.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class BaekJoon_1712 { // 손익분기점
+public class BaekJoon_1712 {  // 손익분기점
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
