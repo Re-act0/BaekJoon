ee374c3110c61a9a54d15a12b450eb14bcb04aff
re_act <re-act@naver.com>
2022-12-06 02:24:42 +0900

    웰컴 / 2022-12-06

---
+++ src/BaekJoon_5337.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class BaekJoon_5337 {  // 웰컴
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(".  .   .\n");
+        bw.write("|  | _ | _. _ ._ _  _\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/.");
+
+        bw.flush();
+    }
+
+
+}
